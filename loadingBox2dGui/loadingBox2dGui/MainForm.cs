3987e6d67e6931e194ca5d3a05303a4fa50dbf16
sehoon <gsh723@naver.com>
2024-10-07 19:10:33 +0900

    set mvp pattern

--- loadingBox2dGui/loadingBox2dGui/MainForm.cs
+++ loadingBox2dGui/loadingBox2dGui/MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using loadingBox2dGui.views;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace loadingBox2dGui
 {
-    public partial class MainForm : Form
+    public partial class MainForm : Form, IMainForm
     {
         public MainForm()
         {
