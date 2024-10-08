bafd08c248a173d668b13cd84e3496d5143180f1
sehoon <gsh723@naver.com>
2024-10-08 19:20:09 +0900

    pylonCommunicator

--- loadingBox2dGui/loadingBox2dGui.models/PylonCommunicator.cs
+++ loadingBox2dGui/loadingBox2dGui.models/PylonCommunicator.cs
@@ -1,7 +1,9 @@
 ﻿using Basler.Pylon;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,75 +11,44 @@ namespace loadingBox2dGui.models
 {
     public class PylonCommunicator
     {
-        public PylonCommunicator() { }
-        public void CatchCamera()
+        Camera _camera1;
+        Camera _camera2;
+
+        public PylonCommunicator()
         {
-            try
+            _camera1 = new Camera();
+            ConnectCamera(_camera1);
+            StartCamera(_camera1);
+
+        }
+
+        public bool ConnectCamera(Camera camera)
+        {
+            camera.Open();
+            return true;
+        }
+        public bool SetCameraSettings(Camera camera)
+        {
+            return true;
+        }
+        public bool StartCamera(Camera camera)
+        {
+            camera.StreamGrabber.Start();
+            IGrabResult grabResult = camera.StreamGrabber.RetrieveResult(1000, TimeoutHandling.ThrowException);
+            if (grabResult.GrabSucceeded)
             {
-                // Create a camera object that selects the first camera device found.
-                // More constructors are available for selecting a specific camera device.
-                using (Camera camera = new Camera())
-                {
-                    // Print the model name of the camera.       
-                    Console.WriteLine("Using camera {0}.", camera.CameraInfo[CameraInfoKey.ModelName]);
-
-                    // Set the acquisition mode to free running continuous acquisition when the camera is opened.
-                    camera.CameraOpened += Configuration.AcquireContinuous;
-
-                    // Open the connection to the camera device.
-                    camera.Open();
-
-                    // The parameter MaxNumBuffer can be used to control the amount of buffers
-                    // allocated for grabbing. The default value of this parameter is 10.
-                    camera.Parameters[PLCameraInstance.MaxNumBuffer].SetValue(5);
-
-                    // Start grabbing.
-                    camera.StreamGrabber.Start();
-
-                    // Grab a number of images.
-                    for (int i = 0; i < 10; ++i)
-                    {
-                        // Wait for an image and then retrieve it. A timeout of 5000 ms is used.
-                        IGrabResult grabResult = camera.StreamGrabber.RetrieveResult(5000, TimeoutHandling.ThrowException);
-                        using (grabResult)
-                        {
-                            // Image grabbed successfully?
-                            if (grabResult.GrabSucceeded)
-                            {
-                                // Access the image data.
-                                Console.WriteLine("SizeX: {0}", grabResult.Width);
-                                Console.WriteLine("SizeY: {0}", grabResult.Height);
-                                byte[] buffer = grabResult.PixelData as byte[];
-                                Console.WriteLine("Gray value of first pixel: {0}", buffer[0]);
-                                Console.WriteLine("");
-
-                                // Display the grabbed image.
-                                ImageWindow.DisplayImage(0, grabResult);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Error: {0} {1}", grabResult.ErrorCode, grabResult.ErrorDescription);
-                            }
-                        }
-                    }
-
-                    // Stop grabbing.
-                    camera.StreamGrabber.Stop();
-
-                    // Close the connection to the camera device.
-                    camera.Close();
-                }
+                Console.WriteLine("SizeX: {0}", grabResult.Width);
+                Console.WriteLine("SizeY: {0}", grabResult.Height);
+                SaveImage(grabResult);
             }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine("Exception: {0}", e.Message);
-            }
-            finally
-            {
-                // Comment the following two lines to disable waiting on exit.
-                Console.Error.WriteLine("\nPress enter to exit.");
-                Console.ReadLine();
-            }
+            return true;
+        }
+
+        public bool SaveImage(IGrabResult grabResult)
+        {
+            string filePath = "output_image.png";
+            //bmp.Save(filePath, ImageFormat.Png);
+            return true;
         }
     }
 }
