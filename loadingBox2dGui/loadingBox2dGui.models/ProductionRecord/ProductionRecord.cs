141ce4dc843139c3857347c7c2d445a6a2f90ff6
IlDoo Park <155957565+ildooPark@users.noreply.github.com>
2025-01-16 15:23:18 +0900

    [TK1][fix] Stablizing CargoBox2D Program throughout event cycles
    [Background]
    Previous push on production record repository did not include newly generated source codes
    
    [changes]
    - pushing source codes added on record repository

---
+++ loadingBox2dGui/loadingBox2dGui.models/ProductionRecord/ProductionRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadingBox2dGui.models.ProductionRecord
+{
+    public class ProductionRecord
+    {
+        public int Id { get; }
+        public int CarType { get; set; }
+        public string CarName { get; set; }
+        public string SequenceNumber { get; set; }
+        public string BodyNumber { get; set; }
+        public InspectionResult InspectionResult { get; set; }
+        public string ScreenShotImagePath { get; set; }
+        public DateTime ProductionDateTime {  get; set; }
+        public double Tx { get; set; }
+        public double Ty { get; set; }
+        public double Rz { get; set; }
+    }
+
+    public enum InspectionResult
+    {
+        NONE = -1,
+        OK = 0,
+        NG = 1,
+        PASS = 2,
+    }
+}
